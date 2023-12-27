ecee5bba15d6ad91e0137345bdd29ff64c817a79
schloing <user.datagram.protocol@proton.me>
2023-12-27 18:03:46 +1100

    crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,27 @@
-use std::io::{self, stdout};
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+use std::io::{self, Write, stdout};
 use crossterm::{
-    event::{self, Event, KeyCode},
-    ExecutableCommand,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
+    cursor,
+    execute,
+    terminal::{enable_raw_mode, size, Clear, ClearType}
 };
-use ratatui::{prelude::*, widgets::*};
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
 
-    let mut should_quit = false;
-    while !should_quit {
-        terminal.draw(ui)?;
-        should_quit = handle_events()?;
-    }
+    let (cols, rows) = size()?;
 
-    disable_raw_mode()?;
-    stdout().execute(LeaveAlternateScreen)?;
-    Ok(())
-}
+    execute!(stdout(), Clear(ClearType::All))?;
 
-fn handle_events() -> io::Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
-        if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(true);
-            }
+    for x in 0..cols {
+        for y in 0..rows {
+            execute!(stdout(), cursor::MoveTo(x, y))?;
+            print!("x");
         }
     }
-    Ok(false)
-}
 
-fn ui(frame: &mut Frame) {
-    frame.render_widget(
-        Paragraph::new("Hello World!")
-        .block(Block::default().title("Greeting").borders(Borders::ALL)),
-        frame.size(),
-        );
+
+    Ok(()) 
 }
