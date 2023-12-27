b03ec7c2f248e33169621289763e6bc3e2bd7dda
schloing <user.datagram.protocol@proton.me>
2023-12-27 19:41:58 +1100

    fun buffer stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+#![allow(dead_code)]
 
 use std::io::{self, Write, stdout};
 use crossterm::{
     cursor,
     execute,
-    terminal::{enable_raw_mode, size, Clear, ClearType}
+    terminal::{enable_raw_mode, size, Clear, ClearType},
+    style::{Color, Attribute, Attributes}
 };
+
+struct Cell {
+    character:  String,
+    foreground: Color,
+    background: Color,
+    attributes: Attributes,
+}
+
+impl Cell {
+    fn default() -> Self {
+        return Cell {
+            character: "".to_string(),
+            foreground: Color::White,
+            background: Color::Black,
+            attributes: Attribute::Reset.into(),
+        }
+    }
+}
+
+struct Buffer {
+    content:    Vec<Cell>,
+    dimensions: (u16, u16),
+}
+
+impl Buffer {
+    fn default() -> Self {
+        let buffsiz     = size().unwrap();
+        let area: usize = (buffsiz.0 * buffsiz.1).into();
+
+        return Buffer {
+            dimensions: buffsiz,
+            content:    Vec::with_capacity(area)
+        }
+    }
+}
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
