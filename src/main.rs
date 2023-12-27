fcf12cc9ca0f87af87e0d08a7833cbedbd4c9751
schloing <user.datagram.protocol@proton.me>
2023-12-27 22:36:21 +1100

    WHY ISNT IT WORKING:

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,47 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use std::io::{self, Write, stdout};
+use std::io::{self, Write, stdout, Stdout};
+use std::ops::Not;
 use crossterm::{
+    QueueableCommand,
     cursor,
     execute,
+    queue,
+    style,
+    style::{Color, Attribute, Attributes, Print},
     terminal::{enable_raw_mode, size, Clear, ClearType},
-    style::{Color, Attribute, Attributes}
 };
 
+#[derive(Clone)]
 struct Cell {
-    character:  String,
-    foreground: Color,
-    background: Color,
-    attributes: Attributes,
+    cchar: char,
+    fg:    Color,
+    bg:    Color,
+    attr:  Attributes,
+}
+
+struct Update {
+    cell:  Cell,
+    index: usize,
 }
 
 impl Cell {
     fn default() -> Self {
-        return Cell {
-            character: "".to_string(),
-            foreground: Color::White,
-            background: Color::Black,
-            attributes: Attribute::Reset.into(),
+        Self {
+            cchar: ' ',
+            fg:    Color::White,
+            bg:    Color::Black,
+            attr:  Attribute::Reset.into(),
         }
     }
 }
 
 struct Buffer {
-    content:    Vec<Cell>,
-    dimensions: (u16, u16),
+    cells:   Vec<Cell>,
+    updates: Vec<Update>,
+    dims:    (u16, u16),
+    write:   Stdout,
 }
 
 impl Buffer {
@@ -38,27 +50,92 @@ impl Buffer {
         let buffsiz     = size().unwrap();
         let area: usize = (buffsiz.0 * buffsiz.1).into();
 
-        return Buffer {
-            dimensions: buffsiz,
-            content:    Vec::with_capacity(area)
+        Self {
+            cells:   vec![Cell::default(); area],
+            updates: Vec::new(),
+            dims:    buffsiz,
+            write:   stdout(),
         }
+    }
+
+    fn add_update(&mut self, cell: Cell, index: usize) {
+        self.updates.push(Update { cell, index });
+    }
+
+    fn render(&mut self) -> io::Result<()> {
+        self.write.queue(Clear(ClearType::All))?;
+        self.write.queue(cursor::MoveTo(0, 0))?;
+
+        let mut foreground = Color::White;
+        let mut background = Color::Black;
+
+        for update in &self.updates {
+            let index = update.index;
+            let cell  = &self.cells[index];
+
+            self.write.queue(cursor::MoveTo(
+                    (index as u16) % self.dims.0,
+                    (index as u16) / self.dims.0,
+                    ))?;
+            if foreground != cell.fg {
+                foreground = cell.fg;
+                self.write.queue(style::SetForegroundColor(foreground))?;
+            }
+
+            if background != cell.bg {
+                background = cell.bg;
+                self.write.queue(style::SetBackgroundColor(background))?;
+            }
+
+            self.write.queue(Print(cell.cchar))?;
+        }
+
+        self.updates.clear();
+        self.write.flush()?;
+
+        Ok(())
     }
 }
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
 
-    let (cols, rows) = size()?;
+    let mut buff = Buffer::default();
 
-    execute!(stdout(), Clear(ClearType::All))?;
+    for _ in 0..5 {
+        for i in 0..buff.cells.len() {
+            let x = (i as u16) % buff.dims.0;
+            let y = (i as u16) / buff.dims.0;
 
-    for x in 0..cols {
-        for y in 0..rows {
-            execute!(stdout(), cursor::MoveTo(x, y))?;
-            print!("x");
+            let fg_color = match i % 3 {
+                0 => Color::Red,
+                1 => Color::Green,
+                _ => Color::Blue,
+            };
+
+            let bg_color = Color::Black;
+
+            let cell_char = match i % 4 {
+                0 => 'A',
+                1 => 'B',
+                2 => 'C',
+                _ => 'D',
+            };
+
+            buff.add_update(
+                Cell {
+                    cchar: cell_char,
+                    fg:    fg_color,
+                    bg:    bg_color,
+                    attr:  Attribute::Reset.into()
+                }, i
+                );
         }
+
+        buff.render()?;
     }
 
+    std::thread::sleep(std::time::Duration::from_secs(5));
 
     Ok(()) 
 }
